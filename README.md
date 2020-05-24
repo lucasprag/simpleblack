e3682a0f8e8392f2e38d315589c6a58666f9f2e3
Lucas Arantes <lucas.ruby.dev@gmail.com>
2020-05-23 22:54:40 -0400

    Add official support to JavaScript

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A black colorscheme for (neo)vim, real black background, #000000.
 
+Support only for Ruby and JavaScript so far. More on the way.
+
 ![simpleblack](github/simpleblack.png)
 
 ## Install
@@ -15,16 +17,20 @@ Plug 'lucasprag/simpleblack'
 You can also set a specific version if you want to not receive updates.
 
 ```
-Plug 'lucasprag/simpleblack', { 'tag': 'v0.1.0' } " latest version
+Plug 'lucasprag/simpleblack', { 'tag': 'v0.2.0' } " latest version
 ```
 
 ## Features
 
-### Ruby
+#### Ruby
 
 Notice that you can distinguish between module, class and method endings by color.
 
 ![simpleblack](github/ruby.png)
+
+#### JavaScript
+
+![simpleblack](github/javascript.png)
 
 ## Usage
 
---
+++ examples/javascript.js
@@ -0,0 +1,36 @@
+
+import { VimColors } from "vimcolors"
+
+class JavaScriptExample extends VimColors {
+  constructor(attributes = {}) {
+    this.colorscheme = attributes.colorscheme;
+  }
+
+  static examples() {
+    // Bacon ipsum dolor sit amet
+    ['string', true, false, null, 99.9, 42, function(){}].forEach(function(value) {
+      console.log(`it appears that ${value} is a ${typeof value}`);
+    })
+
+    Object.entries({string: 'value', number: 2}).forEach((key, value) => {
+      console.warn(`the ${key} key has a value of ${typeof value}`);
+    })
+  }
+
+  present(options) {
+    if (this.colorscheme.match(/dark/)) {
+      return "My colorscheme called " + this.colorscheme + " is dark.";
+    }
+
+    super(options);
+  }
+}
+
+var example = new JavaScriptExample({ colorscheme: "simpleblack" });
+document.getElementById("demo").innerHTML = example.present();
+
+let changeMe = "42"
+const cantChangeMe = 42
+console.log(changeMe = cantChangeMe, changeMe)
+
+
